442c410e1fde725492a41863a31af2fde3fee424
Mithul Mathivanan <mithul.mathivanan@utdallas.edu>
2020-03-04 11:50:56 -0600

    P4

--- kern/monitor.c
+++ kern/monitor.c
@@ -12,10 +12,9 @@
 #include <kern/dwarf.h>
 #include <kern/kdebug.h>
 #include <kern/dwarf_api.h>
-
+#include <kern/trap.h>
 
 #define CMDBUF_SIZE	80	// enough for one VGA text line
-
 
 
 struct Command {
@@ -28,9 +27,6 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
-
-	{ "backtrace", "Display a stack backtrace", mon_backtrace },
-
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
@@ -65,96 +61,9 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-
-	int i;
-	const uint64_t *rbp;
-	uint64_t rip;
-	uint64_t rsp;
-	uint64_t offset;
-	struct Ripdebuginfo info;
-
-	rbp = (const uint64_t*)read_rbp();
-	rsp = read_rsp();
-
-	read_rip(rip);
-
-	cprintf("Stack backtrace:\n");
-	while (rbp) {
-		// print this stack frame
-		cprintf("  rbp %016llx  rip %016llx\n", rbp, rip);
-		if (debuginfo_rip(rip, &info) >= 0){
-			Dwarf_Regtable_Entry *cfa_rule = &info.reg_table.cfa_rule;
-			uint64_t cfa;
-
-			cprintf("       %s:%d: %.*s+%016llx", info.rip_file, info.rip_line, 
-				info.rip_fn_namelen, info.rip_fn_name, rip - info.rip_fn_addr);
-
-			if (cfa_rule->dw_regnum == 6) { /* 6: rbp */
-				cfa = (uint64_t)rbp + cfa_rule->dw_offset;
-			} else if (cfa_rule->dw_regnum == 7) { /* 7: rsp */
-				cfa = rsp + cfa_rule->dw_offset;
-			} else {
-				goto unknown_cfa;
-			}
-
-			cprintf("  args:%d ", info.rip_fn_narg);
-			for (i = 0; i < info.rip_fn_narg ; i++)
-			{
-				uint64_t val;
-				assert(info.offset_fn_arg[i]);
-				offset = cfa + info.offset_fn_arg[i];
-				switch(info.size_fn_arg[i]) {
-					case 8:
-						val = *(uint64_t *) offset;
-						break;
-					case 4:
-						val = *(uint32_t *) offset;
-						break;
-					case 2:
-						val = *(uint16_t *) offset;
-						break;
-					case 1:
-						val = *(uint8_t *) offset;
-						break;
-				}
-				cprintf(" %016x", val);
-			}
-
-			switch(info.reg_table.rules[6].dw_regnum) { /* 6: rbp */
-				case DW_FRAME_SAME_VAL:
-					break;
-				case DW_FRAME_CFA_COL3:
-					rbp = (const uint64_t *)*(uint64_t *)(cfa + info.reg_table.rules[6].dw_offset);
-					break;
-				default:
-					panic("unknown reg rule");
-					break;
-			}
-
-			switch(info.reg_table.rules[16].dw_regnum) { /* 16: rip */
-				case DW_FRAME_SAME_VAL:
-					break;
-				case DW_FRAME_CFA_COL3:
-					rip = *(uint64_t *)(cfa + info.reg_table.rules[16].dw_offset);
-					break;
-				default:
-					panic("unknown reg rule");
-					break;
-			}
-
-			rsp = cfa;
-		} else {
-unknown_cfa:
-			// move to next lower stack frame
-			rip = rbp[1];
-			rbp = (const uint64_t*) rbp[0];
-		}
-		cprintf("\n");
-	}
-
+	// Your code here.
 	return 0;
 }
-
 
 
 
@@ -210,7 +119,8 @@ monitor(struct Trapframe *tf)
 	cprintf("Welcome to the JOS kernel monitor!\n");
 	cprintf("Type 'help' for a list of commands.\n");
 
-
+	if (tf != NULL)
+		print_trapframe(tf);
 
 	while (1) {
 		buf = readline("K> ");
