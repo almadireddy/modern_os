43508f3a34a3da1b6185bdcb3ed0b1ab5c1a554c
Aahlad Madireddy <al.madireddy@gmail.com>
2020-10-02 19:55:11 -0500

    implement env_run()

--- kern/env.c
+++ kern/env.c
@@ -566,6 +566,16 @@ env_run(struct Env *e)
 
     // LAB 3: Your code here.
 
-    panic("env_run not yet implemented");
+    if (curenv && curenv->env_status == ENV_RUNNING) {
+	curenv->env_status = ENV_RUNNABLE;
+    }
+
+    curenv = e;
+    e->env_status = ENV_RUNNING;
+    e->env_runs++;
+
+    lcr3(PADDR(e->env_pml4e));
+    
+    env_pop_tf(&e->env_tf);
 }
 
