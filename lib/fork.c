09c29d98c602c3850ac768870bfd66a1127adf97
alejandroarrdo <alejandroarrdo@gmail.com>
2020-10-31 17:52:28 -0500

    exercise 12

--- lib/fork.c
+++ lib/fork.c
@@ -16,15 +16,26 @@ pgfault(struct UTrapframe *utf)
 {
 	void *addr = (void *) utf->utf_fault_va;
 	uint32_t err = utf->utf_err;
-	int r;
+	//int r; why is this here again?
 
 	// Check that the faulting access was (1) a write, and (2) to a
 	// copy-on-write page.  If not, panic.
 	// Hint:
 	//   Use the read-only page table mappings at uvpt
 	//   (see <inc/memlayout.h>).
+	//
+	//   variables: 
+	//   FEC_WR - Page fault caused by a write
+	//   uvpt - user read only virtual page table 
+	//   page number field of address - PPN(pa) - (((uintptr_t) (pa)) >> PTXSHIFT)
 
 	// LAB 4: Your code here.
+	
+	if(!(err &FEC_WR)&&(uvpt[PPN(addr)] &PTE_COW)){
+		
+		panic("pagefault found by PTE_COW, page isn't writable.");
+
+	}
 
 	// Allocate a new page, map it at a temporary location (PFTEMP),
 	// copy the data from the old page to the new page, then move the new
@@ -35,7 +46,29 @@ pgfault(struct UTrapframe *utf)
 
 	// LAB 4: Your code here.
 
-	panic("pgfault not implemented");
+	void *page_addr;
+	if(sys_page_alloc(0, (void*)PFTEMP, PTE_USER) == 0){
+		
+		page_addr = ROUNDDOWN(addr, PGSIZE);
+		memmove(PFTEMP, page_addr, PGSIZE);
+
+		if(sys_page_map(0, PFTEMP, 0,page_addr, PTE_USER) < 0){
+			
+			panic("sys_page_map has failed at PFTEMP");
+		
+		}
+		if(sys_page_unmap(0, PFTEMP) < 0){
+
+			panic("sys_page_unmap has failed at PFTEMP");
+		}
+
+	}else {
+		
+		panic("sys_page_alloc failed during page_fault");
+
+	}
+	
+	//panic("pgfault not implemented");
 }
 
 //
@@ -52,10 +85,39 @@ pgfault(struct UTrapframe *utf)
 static int
 duppage(envid_t envid, unsigned pn)
 {
-	int r;
+	//int r; better off renaming as perm
 
 	// LAB 4: Your code here.
-	panic("duppage not implemented");
+	
+	int perm = (uvpt[pn])&0x1FF; //use page directory index
+	void *addr = (void *)((uint64_t)pn *PGSIZE);
+
+	if(perm &PTE_W || perm &PTE_COW){
+
+		perm = ((perm | (PTE_P|PTE_U|PTE_COW)) &(~PTE_W));
+
+		if(sys_page_map(0, addr, envid, addr, perm) < 0){
+			
+			panic("sys_page_map has failed with PTE_COW");
+		
+		}
+		if(sys_page_map(0, addr, 0, addr, perm) < 0){
+		
+			panic("sys_page_map has failed with PTE_COW");
+		
+		}
+
+	} else{
+
+		if(sys_page_map(0, addr, envid, addr, perm) < 0){
+			
+			panic("sys_page_map has failed with PTE_COW");
+		
+		}
+		
+	}
+
+	//panic("duppage not implemented");
 	return 0;
 }
 
@@ -79,8 +141,70 @@ envid_t
 fork(void)
 {
 	// LAB 4: Your code here.
-	panic("fork not implemented");
+	
+	envid_t envid;
+	extern void _pgfault_upcall(void);
+	uint8_t *addr;
+	extern char end[];
+	set_pgfault_handler(pgfault);
+
+	envid = sys_exofork();
+	if (envid < 0){
+
+		panic("envid less than 0, process creation has failed.");
+	}
+	else if (envid == 0){
+
+		thisenv = &envs[ENVX(sys_getenvid())];
+		return envid;
+	}
+	else {
+
+		for (addr = (uint8_t*)USTACKTOP - PGSIZE; addr >= (uint8_t*)UTEXT; addr -= PGSIZE){
+
+			if(uvpml4e[VPML4E(addr) & PTE_P]){
+
+				if(uvpde[VPDPE(addr) & PTE_P]){
+
+					if(uvpd[VPD(addr) & PTE_P]){
+
+						if(uvpt[VPN(addr) & PTE_P]){
+							
+							duppage(envid, VPN(addr));
+						}
+					} else {
+						
+						addr -= NPDENTRIES*PGSIZE;
+					}
+				} else{
+
+					addr -= NPDENTRIES*NPDENTRIES*PGSIZE;
+				}
+			}
+
+		}
+	}
+
+	if (sys_env_set_pgfault_upcall(envid, (void*)_pgfault_upcall) < 0){
+		
+		panic("sys_env_set_pgfault has failed.");
+	}
+	if (sys_page_alloc(envid, (void*)UXSTACKTOP - PGSIZE, PTE_USER) < 0){
+		
+		panic("sys_page_alloc has failed.");
+	}
+	if (sys_env_set_status(envid, ENV_RUNNABLE)){
+
+		panic("sys_env_set_status has failed.");
+	}
+
+	return envid;
+
+
+	//panic("fork not implemented");
 }
+
+//function created to implement asm and retrieve esp
 
 // Challenge!
 int
